522f371ca52f3359120e2b99850a44e418b3ddb1
Shreyanshs0ni <shreyanshsoni07@gmail.com>
2025-08-14 14:42:50 +0530

    madlibs completed

---
+++ madlibs/app.py
@@ -0,0 +1,16 @@
+
+adjective = input("Adjective: ");
+adjective2 = input("Adjective-2: ");
+
+adverb = input("Adverb: ");
+
+verb = input("Verb: ");
+verb2 = input("Verb-2: ");
+
+noun = input("Noun: ");
+noun2 = input("Noun-2: ");
+noun3 = input("Noun-3: ");
+
+
+
+print(f"On a {adjective} morning, I {adverb} {verb} to the {noun}, hoping to find a {adjective2} {noun2} that could {verb2} my {noun3}.");
