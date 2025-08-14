4f6fe5c2aaee15d14d703e888a5b3c54159270c8
Shreyanshs0ni <shreyanshsoni07@gmail.com>
2025-08-14 14:55:48 +0530

    guessthenumber completed

---
+++ guessTheNumber/app.py
@@ -0,0 +1,15 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1,x)
+    guess = 0
+    while guess!=random_number:
+        guess = int(input(f"guess a number between 1 and {x} : "))
+        if(random_number > guess):
+            print("Higher");
+        elif(random_number < guess):
+            print("Lower");
+        else:
+            print("Woohhooo!!");
+
+guess(200)
